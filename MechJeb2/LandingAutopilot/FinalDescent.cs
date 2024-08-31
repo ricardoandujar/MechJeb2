3f8154cd247ee0129eb07e49ff618e31a5a6ca8f
ricardoandujar <ricardo.andujar@gmail.com>
2024-08-31 00:03:20 -0500

    Draft changes for Landing Autopilot
    Updated landing autopilot to work for both standard KSP and RSS. Introduced the 'Fly Safely' selection option as the default. When disabled follows a more aggressive landing but has more chance of crashing into surface.
    Updated Localization update for Safe Mode
    
        For RSS, successfully landed on:
           Earth, Moon, Mars
        Still working it to get it land successfully on Phobos & Deimos
    
        For std KSP, successfully landed on:
           Mun, Gilly.  Still need to improve Gilly landing before testing on RSS.

--- MechJeb2/LandingAutopilot/FinalDescent.cs
+++ MechJeb2/LandingAutopilot/FinalDescent.cs
@@ -1,5 +1,8 @@
+﻿#define SIMPLE
+
 using System;
 using KSP.Localization;
+using ModuleWheels;
 using UnityEngine;
 
 namespace MuMech
@@ -8,26 +11,112 @@ namespace MuMech
     {
         public class FinalDescent : AutopilotStep
         {
+            private const float FINAL_ALT_THRESHOLD_CONSTANT = 4000.0f;//400.0F;
+            private const float LANDING_GEAR_ALT_CONSTANT = 200.0F;
+            private const float MIN_HORIZONTAL_ALT_THRESHOLD_CONSTANT = FINAL_ALT_THRESHOLD_CONSTANT;
+            private const float MAX_HORIZONTAL_ALT_THRESHOLD_CONSTANT = 7000.0F;
+            private const float ABOVE_FINAL_ALT_SPEED_CONSTANT = -40.0F;
+            private const float MIN_WARP_ALT_THRESHOLD_CONSTANT = MAX_HORIZONTAL_ALT_THRESHOLD_CONSTANT;
+            private const float WARP_EDGE_THRESHOLD_CONSTANT = 1.02F;
+            private const float ANGLE_MIN_SPEED_RATIO_CONSTANT = 1.3F;
+            private const float ANGLE_MIN_ANGLE_RATIO_CONSTANT = 0.0F;
+            private const float ANGLE_MAX_ANGLE_RATIO_CONSTANT = 0.6F;
+            private const float ANGLE_ADJUST_I_CONSTANT = 0.00001f;
+            private const float ANGLE_ADJUST_P_CONSTANT = -0.01f;
+            private const float FINAL_SPEED_FACTOR_CONSTANT = 0.8F;
+            private const float MAX_WARP_ANGLE_CONSTANT = 45.0F;
+            private bool        _deployedGears;
+            private float       magnitude = 0;
             private IDescentSpeedPolicy _aggressivePolicy;
+            private bool        warp = false;
+            private bool        useRealAlt = false;
 
-            public FinalDescent(MechJebCore core) : base(core)
+            public FinalDescent(MechJebCore core, float _TargetThrottle) : base(core)
             {
+                _deployedGears = false;
+                Core.Thrust.TargetThrottle = _TargetThrottle;
+            }
+
+            private double GetMinAlt()
+            {
+                double minalt;
+                if ( useRealAlt == false )
+                {
+                    minalt = Math.Min(VesselState.altitudeBottom, Math.Min(VesselState.altitudeASL, VesselState.altitudeTrue));
+                    if ( minalt < 5 )
+                    {
+                        useRealAlt = true;
+                    }
+                }
+                else
+                {
+                    minalt = VesselState.altitudeTrue;
+                }
+                return minalt;
+            }
+
+            private double GetMaxSpeed(bool updatePolicy, double minalt )
+            {
+                double maxSpeed;
+
+                if (minalt > FINAL_ALT_THRESHOLD_CONSTANT)
+                {
+                    if (Core.Landing.FlySafe)
+                    {
+                        maxSpeed = Math.Sqrt((VesselState.limitedMaxThrustAccel - VesselState.localg) * 2 * minalt);
+                    }
+                    else
+                    {
+                        if (updatePolicy || _aggressivePolicy == null)
+                        {
+                            Vector3d estimatedLandingPosition = VesselState.CoM + VesselState.surfaceVelocity.sqrMagnitude / (2 * VesselState.limitedMaxThrustAccel) * VesselState.surfaceVelocity.normalized;
+                            double terrainRadius = MainBody.Radius + MainBody.TerrainAltitude(estimatedLandingPosition);
+                            _aggressivePolicy = new GravityTurnDescentSpeedPolicy(terrainRadius, MainBody.GeeASL * 9.81, VesselState.limitedMaxThrustAccel); // this constant policy creation is wastefull...
+                        }
+                        maxSpeed = _aggressivePolicy.MaxAllowedSpeed(VesselState.CoM - MainBody.position, VesselState.surfaceVelocity);
+                        maxSpeed = Math.Max(maxSpeed, Math.Sqrt((VesselState.limitedMaxThrustAccel - VesselState.localg) * 2 * minalt));
+                    }
+                }
+                else
+                {
+                    maxSpeed = Mathf.Lerp(0,
+                        (float)Math.Sqrt((VesselState.limitedMaxThrustAccel - VesselState.localg) * 2 * FINAL_ALT_THRESHOLD_CONSTANT) * FINAL_SPEED_FACTOR_CONSTANT, (float)VesselState.altitudeTrue / FINAL_ALT_THRESHOLD_CONSTANT);
+                }
+
+                return maxSpeed;
             }
 
             public override AutopilotStep OnFixedUpdate()
             {
-                double minalt = Math.Min(VesselState.altitudeBottom, Math.Min(VesselState.altitudeASL, VesselState.altitudeTrue));
+                double minalt = GetMinAlt();
+                double maxSpeed = GetMaxSpeed(false, minalt);
 
-                if (!Core.Node.Autowarp || _aggressivePolicy == null) return this;
-
-                double maxVel = 1.02 * _aggressivePolicy.MaxAllowedSpeed(VesselState.CoM - MainBody.position, VesselState.surfaceVelocity);
+                if (!Core.Node.Autowarp || (minalt < MIN_WARP_ALT_THRESHOLD_CONSTANT) || (maxSpeed < VesselState.speedSurface))
+                {
+                    if ( warp == true )
+                    {
+                        warp = false;
+                        Core.Warp.MinimumWarp(true);
+                    }
+                    return this;
+                }
+                double maxVel = WARP_EDGE_THRESHOLD_CONSTANT * maxSpeed;
 
                 double diffPercent = (maxVel / VesselState.speedSurface - 1) * 100;
 
-                if (minalt > 200 && diffPercent > 0 && Vector3d.Angle(VesselState.forward, -VesselState.surfaceVelocity) < 45)
+                if ( diffPercent > 0 ) //&& Vector3d.Angle(VesselState.forward, -VesselState.surfaceVelocity) < MAX_WARP_ANGLE_CONSTANT)
+                {
+                    warp = true;
                     Core.Warp.WarpRegularAtRate((float)(diffPercent * diffPercent * diffPercent));
+                }
                 else
-                    Core.Warp.MinimumWarp(true);
+                {
+                    if (warp == true)
+                    {
+                        warp = false;
+                        Core.Warp.MinimumWarp(true);
+                    }
+                }
 
                 return this;
             }
@@ -42,93 +131,89 @@ namespace MuMech
 
                 // TODO perhaps we should pop the parachutes at this point, or at least consider it depending on the altitude.
 
-                double minalt = Math.Min(VesselState.altitudeBottom, Math.Min(VesselState.altitudeASL, VesselState.altitudeTrue));
+                double minalt = GetMinAlt();
 
-                if (VesselState.limitedMaxThrustAccel < VesselState.gravityForce.magnitude)
+                // Consider lowering the langing gear
+                if (!_deployedGears)
                 {
-                    // if we have TWR < 1, just try as hard as we can to decelerate:
-                    // (we need this special case because otherwise the calculations spit out NaN's)
+                    if (VesselState.altitudeTrue < LANDING_GEAR_ALT_CONSTANT )
+                    {
+                        Vessel.ActionGroups.SetGroup(KSPActionGroup.Gear, true);
+                        _deployedGears = true;
+                    }
+                }
+
+                // if we have TWR < 1, just try as hard as we can to decelerate:
+                // (we need this special case because otherwise the calculations spit out NaN's)
+                // Also used to kill horizontal velocity before we do final approach. 
+                if ( (VesselState.limitedMaxThrustAccel < VesselState.localg ) ||
+                     ((VesselState.speedSurfaceHorizontal > 5) && (minalt > MIN_HORIZONTAL_ALT_THRESHOLD_CONSTANT) && (minalt < MAX_HORIZONTAL_ALT_THRESHOLD_CONSTANT)) )
+                {
                     Core.Thrust.Tmode       = MechJebModuleThrustController.TMode.KEEP_VERTICAL;
                     Core.Thrust.TransKillH  = true;
-                    Core.Thrust.TransSpdAct = 0;
+                    Core.Thrust.TransSpdAct = 5.0F;
                 }
-                else if (minalt > 200)
+
+                // Point Up and set speed to -2 meters/sec
+                // This ensures that surface retrograde is never activated when close to zero or positive vertical
+                // speed.
+                else if ( (VesselState.speedVertical >= -0.1) && (minalt < FINAL_ALT_THRESHOLD_CONSTANT) )
                 {
-                    if (VesselState.surfaceVelocity.magnitude > 5 && Vector3d.Angle(VesselState.surfaceVelocity, VesselState.up) < 80)
-                    {
-                        // if we have positive vertical velocity, point up and don't thrust:
-                        Core.Attitude.attitudeTo(Vector3d.up, AttitudeReference.SURFACE_NORTH, null);
-                        Core.Thrust.Tmode       = MechJebModuleThrustController.TMode.DIRECT;
-                        Core.Thrust.TransSpdAct = 0;
-                    }
-                    else if (VesselState.surfaceVelocity.magnitude > 5 && Vector3d.Angle(VesselState.forward, -VesselState.surfaceVelocity) > 45)
-                    {
-                        // if we're not facing approximately retrograde, turn to point retrograde and don't thrust:
-                        Core.Attitude.attitudeTo(Vector3d.back, AttitudeReference.SURFACE_VELOCITY, null);
-                        Core.Thrust.Tmode       = MechJebModuleThrustController.TMode.DIRECT;
-                        Core.Thrust.TransSpdAct = 0;
-                    }
-                    else
-                    {
-                        //if we're above 200m, point retrograde and control surface velocity:
-                        Core.Attitude.attitudeTo(Vector3d.back, AttitudeReference.SURFACE_VELOCITY, null);
-
-                        Core.Thrust.Tmode = MechJebModuleThrustController.TMode.KEEP_SURFACE;
-
-                        //core.thrust.trans_spd_act = (float)Math.Sqrt((vesselState.maxThrustAccel - vesselState.gravityForce.magnitude) * 2 * minalt) * 0.90F;
-                        Vector3d estimatedLandingPosition = VesselState.CoM + VesselState.surfaceVelocity.sqrMagnitude /
-                            (2 * VesselState.limitedMaxThrustAccel) * VesselState.surfaceVelocity.normalized;
-                        double terrainRadius = MainBody.Radius + MainBody.TerrainAltitude(estimatedLandingPosition);
-                        _aggressivePolicy =
-                            new GravityTurnDescentSpeedPolicy(terrainRadius, MainBody.GeeASL * 9.81,
-                                VesselState.limitedMaxThrustAccel); // this constant policy creation is wastefull...
-                        Core.Thrust.TransSpdAct =
-                            (float)_aggressivePolicy.MaxAllowedSpeed(VesselState.CoM - MainBody.position, VesselState.surfaceVelocity);
-                    }
+                    // if we have positive vertical velocity, point up and don't thrust:
+                    Core.Attitude.attitudeTo(Vector3d.up, AttitudeReference.SURFACE_NORTH, null);
+                    Core.Thrust.Tmode = MechJebModuleThrustController.TMode.DIRECT;
+                    Core.Thrust.TransSpdAct = -2;
                 }
+
+                // Perform Surface Retrograde using calculated speed
+                // if we're falling at a significant angle from vertical, our vertical speed might be
+                // quite small but we might still need to decelerate. Control the total speed instead
+                // by thrusting directly retrograde
                 else
                 {
-                    // last 200 meters:
-                    Core.Thrust.TransSpdAct = -Mathf.Lerp(0,
-                        (float)Math.Sqrt((VesselState.limitedMaxThrustAccel - VesselState.localg) * 2 * 200) * 0.90F, (float)minalt / 200);
+                    // Get Max Speed - based on altitude + FlySafe setting
+                    Core.Thrust.TransSpdAct = -(float)GetMaxSpeed(true, minalt);
 
                     // take into account desired landing speed:
                     Core.Thrust.TransSpdAct = (float)Math.Min(-Core.Landing.TouchdownSpeed, Core.Thrust.TransSpdAct);
 
-//                    core.thrust.tmode = MechJebModuleThrustController.TMode.KEEP_VERTICAL;
-//                    core.thrust.trans_kill_h = true;
-
-//                    if (Math.Abs(Vector3d.Angle(-vessel.surfaceVelocity, vesselState.up)) < 10)
-                    if (VesselState.speedSurfaceHorizontal < 5)
+                    // If the surfaceSpeed is significantly larger then the targeted speed
+                    // Attempt to minimize altitude loss to allow more time to burn the horizontal speed, othwerwise
+                    // the vessel will crash into terrain.
+                    // This is is definitely needed when starting from a low orbit with a low TWR.
+                    Vector3d point;
+                    if ((minalt > MAX_HORIZONTAL_ALT_THRESHOLD_CONSTANT) && VesselState.speedSurface > Math.Abs(Core.Thrust.TransSpdAct)*ANGLE_MIN_SPEED_RATIO_CONSTANT)
                     {
-                        // if we're falling more or less straight down, control vertical speed and
-                        // kill horizontal velocity
-                        Core.Thrust.Tmode      = MechJebModuleThrustController.TMode.KEEP_VERTICAL;
-                        Core.Thrust.TransKillH = true;
+                        magnitude -= ANGLE_ADJUST_I_CONSTANT * ((float)VesselState.speedVertical);
+                        magnitude = Mathf.Min(ANGLE_MAX_ANGLE_RATIO_CONSTANT, Mathf.Max(ANGLE_MIN_ANGLE_RATIO_CONSTANT, magnitude));
+                        point = Vector3d.Lerp(Vector3d.back, Vector3d.up, magnitude+(ANGLE_ADJUST_P_CONSTANT * ((float)VesselState.speedVertical)) );
+                        Core.Thrust.TransKillH = false;
                     }
+
+                    // No correction needed so use retrograde unchanged
                     else
                     {
-                        // if we're falling at a significant angle from vertical, our vertical speed might be
-                        // quite small but we might still need to decelerate. Control the total speed instead
-                        // by thrusting directly retrograde
-                        Core.Attitude.attitudeTo(Vector3d.back, AttitudeReference.SURFACE_VELOCITY, null);
-                        Core.Thrust.Tmode       =  MechJebModuleThrustController.TMode.KEEP_SURFACE;
-                        Core.Thrust.TransSpdAct *= -1;
+                        magnitude = 0.0f;
+                        point = Vector3d.back;
+                        if ((VesselState.speedSurfaceHorizontal < 5) && (minalt < FINAL_ALT_THRESHOLD_CONSTANT))
+                        {
+                            Core.Thrust.TransKillH = true;
+                        }
+                        else
+                        {
+                            Core.Thrust.TransKillH = false;
+                        }
                     }
+
+                    Core.Attitude.attitudeTo(point, AttitudeReference.SURFACE_VELOCITY, null);
+                    Core.Thrust.Tmode       =  MechJebModuleThrustController.TMode.KEEP_SURFACE;
+                    Core.Thrust.TransSpdAct *= -1;
                 }
 
-                Status = Localizer.Format("#MechJeb_LandingGuidance_Status9",
-                    VesselState.altitudeBottom.ToString("F0")); //"Final descent: " +  + "m above terrain"
-
-                // ComputeCourseCorrection doesn't work close to the ground
-                /* if (core.landing.landAtTarget)
-                {
-                    core.rcs.enabled = true;
-                    Vector3d deltaV = core.landing.ComputeCourseCorrection(false);
-                    core.rcs.SetWorldVelocityError(deltaV);
-
-                    status += "\nDV: " + deltaV.magnitude.ToString("F3") + " m/s";
-                } */
+                // Display target speed rather than altitude since that is already available elsewhere - this
+                // allows for monitoring progress.
+                Status = Localizer.Format("#MechJeb_LandingGuidance_Status6",
+                    Core.Thrust.TransSpdAct >= double.MaxValue ? "∞" : Core.Thrust.TransSpdAct.ToString("F1")); //"Braking: target speed = " +  + " m/s"
 
                 return this;
             }
--- MechJeb2/LandingAutopilot/KillHorizontalVelocity.cs
+++ MechJeb2/LandingAutopilot/KillHorizontalVelocity.cs
@@ -1,4 +1,4 @@
-using KSP.Localization;
+﻿using KSP.Localization;
 using UnityEngine;
 
 namespace MuMech
@@ -7,32 +7,42 @@ namespace MuMech
     {
         public class KillHorizontalVelocity : AutopilotStep
         {
-            public KillHorizontalVelocity(MechJebCore core) : base(core)
+            public KillHorizontalVelocity(MechJebCore core, float _TargetThrottle) : base(core)
             {
+                Core.Thrust.TargetThrottle = _TargetThrottle;
             }
 
             // TODO I think that this function could be better rewritten to much more agressively kill the horizontal velocity. At present on low gravity bodies such as Bop, the craft will hover and slowly drift sideways, loosing the prescion of the landing. 
             public override AutopilotStep Drive(FlightCtrlState s)
             {
+                // If Horizontal speed is already below threshold then move on to Final Descent.
+                if (VesselState.speedSurfaceHorizontal < 5)
+                {
+                    return new FinalDescent(Core, Core.Thrust.TargetThrottle);
+                }
+
                 if (!Core.Landing.PredictionReady)
                     return this;
 
                 Vector3d horizontalPointingDirection = Vector3d.Exclude(VesselState.up, VesselState.forward).normalized;
+
+                // If Vertical speed is positive then time to move on to final descent - it will kill any residual horizontal speed.
                 if (Vector3d.Dot(horizontalPointingDirection, VesselState.surfaceVelocity) > 0)
                 {
                     Core.Thrust.TargetThrottle = 0;
                     Core.Attitude.attitudeTo(Vector3.up, AttitudeReference.SURFACE_NORTH, Core.Landing);
-                    return new FinalDescent(Core);
+                    return new FinalDescent(Core, Core.Thrust.TargetThrottle);
                 }
 
                 //control thrust to control vertical speed:
-                const double DESIRED_SPEED = 0; //hover until horizontal velocity is killed
+                const double DESIRED_SPEED = 0.0F; //hover until horizontal velocity is killed
                 double controlledSpeed = Vector3d.Dot(VesselState.surfaceVelocity, VesselState.up);
                 double speedError = DESIRED_SPEED - controlledSpeed;
-                const double SPEED_CORRECTION_TIME_CONSTANT = 1.0;
+                const double SPEED_CORRECTION_TIME_CONSTANT = 1.0F;
+                const double ANGLE_FACTOR_CONSTANT = 0.3F;
                 double desiredAccel = speedError / SPEED_CORRECTION_TIME_CONSTANT;
                 double minAccel = -VesselState.localg;
-                double maxAccel = -VesselState.localg + Vector3d.Dot(VesselState.forward, VesselState.up) * VesselState.maxThrustAccel;
+                double maxAccel = -VesselState.localg + Vector3d.Dot(VesselState.forward, VesselState.up) * VesselState.limitedMaxThrustAccel;
                 if (maxAccel - minAccel > 0)
                 {
                     Core.Thrust.TargetThrottle = Mathf.Clamp((float)((desiredAccel - minAccel) / (maxAccel - minAccel)), 0.0F, 1.0F);
@@ -43,7 +53,7 @@ namespace MuMech
                 }
 
                 //angle up and slightly away from vertical:
-                Vector3d desiredThrustVector = (VesselState.up + 0.2 * horizontalPointingDirection).normalized;
+                Vector3d desiredThrustVector = (VesselState.up + ANGLE_FACTOR_CONSTANT * horizontalPointingDirection).normalized;
 
                 Core.Attitude.attitudeTo(desiredThrustVector, AttitudeReference.INERTIAL, Core.Landing);
 
